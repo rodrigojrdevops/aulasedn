65174e537837c4488169468ed572c984c3775612
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-05-08 19:40:30 -0300

    Create calculadora_idade_dias.py

---
+++ Aula 05/calculadora_idade_dias.py
@@ -0,0 +1,16 @@
+def calcular_idade_em_dias(ano_nascimento):
+    """Calcula a idade das pessoasa em dias (Aproximado)
+    
+    
+    Params:
+        ano_nascimento (int):
+        
+"""
+    Args:
+        ano_nascimento (_type_): _description_
+        
+        #obtendo ano atual
+        
+        ano_atual = datetime.datetime.now().year
+        
+  
